11a43534028bba24bff84f58ad467bed77f8727d
Sander van den Hoed <s.vdhoed@gmail.com>
2020-08-04 12:23:47 +0200

    Updated readability

--- Extensions/SN_SlashCommands.lua
+++ Extensions/SN_SlashCommands.lua
@@ -1,7 +1,9 @@
 function addonCommand(command)
     if      command == "show"   then SN_ItemList:Show()
     elseif  command == "reset"  then SN_ItemList:Reset()
-    else                             SN:PrintMsg("Available commands are:\n/sn show - Starts tracking and shows loot being distributed.\n/sn reset - Resets the list of loot being distributed.")
+    else                             SN:PrintMsg("Available commands are:"
+        .."\n/sn show - Starts tracking and shows loot being distributed."
+        .."\n/sn reset - Resets the list of loot being distributed.")
     end
 end
 
