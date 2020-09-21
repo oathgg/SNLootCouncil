4d6ae7d8edc8abdc1f7ed760222fdc558299946d
Sander van den Hoed <s.vdhoed@gmail.com>
2020-09-21 11:49:20 +0200

    Split logic behind the loot distrubtion.

--- SN_ItemTracker.lua
+++ SN_ItemTracker.lua
@@ -3,34 +3,43 @@ SN_ItemTracker = {}
 local IS_ENABLED = false
 local PREV_ITEM = ""
 
-function OnItemDistribution(...)
-    if IS_ENABLED then
+local function IsValidDistribution(...)
+    local targetPlayer, itemName, quality = ...
+
+    return IS_ENABLED and targetPlayer and itemName and quality and itemName ~= "Nexus Crystal" and quality >= SN.MinimumItemRarityBeforeProcessing
+end
+
+-- When we trade the item to someone else this function will trigger.
+-- We check if we have an item in our already created list with our own name, if not then we quickly add this item to our list and assign the owner.
+function OnItemTradeDistribution(...)
+    if IsValidDistribution(...) then
         local targetPlayer, itemName, quality = ...
 
-        -- Ignore these items
-        if itemName == "Nexus Crystal" then 
-            return
+        local itemId = SN_Item:GetItemByNameWithMyselfAsOwner(itemName)
+        if not itemId then
+            SN:PrintMsg("Couldn't find the traded item in the list.")
+        else
+            SN_Item:AssignOwner(itemId, targetPlayer)
+            SN:PrintMsg("Distributed "..itemName.." to "..targetPlayer)
+        end
+    end
+end
+
+function OnItemMLDistribution(...)
+    if IsValidDistribution(...) then
+        local targetPlayer, itemName, quality = ...
+
+        local itemId = SN_Item:GetItemByNameWithoutOwner(itemName)
+
+        -- BUG:: In case we somehow didn't catch the raid warning we will create it again.
+        if not itemId then
+            SN:PrintMsg("Couldn't find an item in the list for name '"..itemName.."', creating item.")
+            SN_ItemList:Add(itemName)
+            itemId = SN_Item:GetItemByNameWithoutOwner(itemName)
         end
 
-        if targetPlayer and itemName and quality then
-            if quality >= SN.MinimumItemRarityBeforeProcessing then
-                -- We will find something if it's being distributed through the master loot function
-                local itemId = SN_Item:GetItemByNameWithoutOwner(itemName)
-
-                -- We most likely looted it ourself so we can distribute it later through a trade.
-                -- If we don't have an item AND we're trading it then we should look it up in the list with ourselves as owner.
-                if not itemId and targetPlayer ~= UnitName("player") then
-                    itemId = SN_Item:GetItemByNameWithMyselfAsOwner(itemName)
-                end
-
-                if itemId then
-                    SN_Item:AssignOwner(itemId, targetPlayer)
-                    SN:PrintMsg("Distributed "..itemName.." to "..targetPlayer)
-                else
-                    SN:PrintMsg("Couldn't find an item in the list for name "..itemName)
-                end
-            end
-        end
+        SN_Item:AssignOwner(itemId, targetPlayer)
+        SN:PrintMsg("Distributed "..itemName.." to "..targetPlayer)
     end
 end
 
@@ -52,8 +61,8 @@ function SN_ItemTracker:Start()
     if not IS_ENABLED then
         SN:PrintMsg("Started tracker")
         SN_ChatReader:Start()
-        SN_TradeTracker:SubscribeSuccessCallback(OnItemDistribution)
-        SN_MasterLoot:SubscribeCallback(OnItemDistribution)
+        SN_TradeTracker:SubscribeSuccessCallback(OnItemTradeDistribution)
+        SN_MasterLoot:SubscribeCallback(OnItemMLDistribution)
         SN_ChatReader:SubscribeCallback(OnChatDistribution)
 
         IS_ENABLED = true
