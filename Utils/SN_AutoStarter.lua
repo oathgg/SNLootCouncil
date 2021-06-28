972ca49ba2e714d1641f19abfc6ab60601b089ef
Sander van den Hoed <s.vdhoed@gmail.com>
2021-06-28 18:53:10 +0200

    Removed stopped logging combat message

--- Utils/SN_AutoStarter.lua
+++ Utils/SN_AutoStarter.lua
@@ -4,14 +4,13 @@ function SN_AutoStarterEvents.PLAYER_ENTERING_WORLD(...)
     local inInstance, instanceType = IsInInstance()
     if inInstance and instanceType == "raid" then
         local instanceName, _, _, _, _ = GetInstanceInfo()
-        SN_ItemTracker:Start()
         SN:PrintMsg("Player entered "..instanceName)
+        SN_ItemTracker:Start()
+        
         SN:PrintMsg("Combat logging started.")
         LoggingCombat(true)
     else
         SN_ItemTracker:Stop()
-
-        SN:PrintMsg("Combat logging stopped.")
         LoggingCombat(false)
     end
 end
